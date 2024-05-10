a9ac5dff3806483ac2b8d466099ebdbfc567e4a7
AaronRuizMoraUK <aaronruizmora@gmail.com>
2024-05-10 01:33:56 +0100

    Include vectorial target, which is necessary when mathfu library uses SIMD.

--- Scripts/FetchLibraries.cmake
+++ Scripts/FetchLibraries.cmake
@@ -28,6 +28,7 @@ set_target_properties(update_mappings PROPERTIES FOLDER ${glfw_folder})
 # Provides a lightweight and efficient implementation of vectors,
 # matrices and quaternions.
 # ---------------------------------------------------------------------
+set(mathfu_enable_simd ON CACHE INTERNAL "Turn on SIMD")
 set(mathfu_build_benchmarks OFF CACHE INTERNAL "Turn off benchmarks")
 set(mathfu_build_tests OFF CACHE INTERNAL "Turn off tests")
 set(mathfu_patch git apply ${CMAKE_SOURCE_DIR}/Scripts/Patches/mathfu.patch)
@@ -36,19 +37,25 @@ FetchContent_Declare(
     mathfu
     GIT_REPOSITORY https://github.com/google/mathfu.git
     GIT_TAG da23a1227bb65fbb7f2f5b6c504fbbdd1dfdab4b # Last commit on master (9th May 2022)
-    PATCH_COMMAND ${mathfu_patch} # Patch to fix std::max/min usage and increase cmake min version
+    PATCH_COMMAND ${mathfu_patch} # Patch to fix std::max/min usage and increase CMake min version
     UPDATE_DISCONNECTED 1 # Avoid to apply the patch again once it has been populated
 )
 
 FetchContent_MakeAvailable(mathfu)
 
+file(GLOB_RECURSE VECTORIAL_SOURCE_FILES
+    "${mathfu_SOURCE_DIR}/dependencies/vectorial/include/*.*")
+source_group(TREE "${mathfu_SOURCE_DIR}/dependencies/vectorial/include" FILES ${VECTORIAL_SOURCE_FILES})
+add_library(vectorial INTERFACE ${VECTORIAL_SOURCE_FILES})
+target_include_directories(vectorial INTERFACE "${mathfu_SOURCE_DIR}/dependencies/vectorial/include") 
+
 file(GLOB_RECURSE MATHFU_SOURCE_FILES
     "${mathfu_SOURCE_DIR}/include/*.*")
-
 source_group(TREE "${mathfu_SOURCE_DIR}/include" FILES ${MATHFU_SOURCE_FILES})
-
 add_library(mathfu INTERFACE ${MATHFU_SOURCE_FILES})
 target_include_directories(mathfu INTERFACE "${mathfu_SOURCE_DIR}/include") 
+target_link_libraries(mathfu INTERFACE vectorial)
 
 set(mathfu_folder "${3rdparty_folder}/mathfu")
 set_target_properties(mathfu PROPERTIES FOLDER ${mathfu_folder})
+set_target_properties(vectorial PROPERTIES FOLDER ${mathfu_folder})
