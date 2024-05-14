1d7a282377f22b7b56dd05ae57ee530ff4ff05dc
AaronRuizMoraUK <aaronruizmora@gmail.com>
2024-05-14 18:18:41 +0100

    Improve logging by printing in the visual studio output window too.

--- Source/Core/Source/Log/Log.cpp
+++ Source/Core/Source/Log/Log.cpp
@@ -4,27 +4,64 @@
 #include <cstdarg>
 #include <cassert>
 
+#ifdef _WIN32
+#include <Windows.h>
+#endif
+
 namespace DX::Internal
 {
     namespace
     {
-        constexpr const char* LogNormal = "\033[39m";
-        constexpr const char* LogYellow = "\033[33m";
-        constexpr const char* LogRed = "\033[31m";
+        enum class LogColor
+        {
+            Normal,
+            Yellow,
+            Red
+        };
+
+        void DebugOutput(LogColor logColor, const char* message)
+        {
+            auto colorStr = [](LogColor logColor)
+                {
+                    constexpr const char* LogNormal = "\033[39m";
+                    constexpr const char* LogYellow = "\033[33m";
+                    constexpr const char* LogRed = "\033[31m";
+
+                    switch (logColor)
+                    {
+                    case LogColor::Normal: return LogNormal;
+                    case LogColor::Yellow: return LogYellow;
+                    case LogColor::Red: return LogRed;
+                    default: return LogNormal;
+                    }
+                };
+
+            std::printf("%s%s%s", colorStr(logColor), message, colorStr(LogColor::Normal));
+#ifdef _WIN32
+            OutputDebugString(message);
+#endif
+        }
     }
 
     void Log(LogLevel level, const char* title, const char* message, ...)
     {
+        char buffer[8 * 1024];
+        char* head = buffer;
+
+        LogColor logColor = LogColor::Normal;
+
         switch(level)
         {
             case LogLevel::Info:
-                std::printf("[%s] ", title);
+                head += std::sprintf(head, "[%s] ", title);
                 break;
             case LogLevel::Warning:
-                std::printf("%s[%s] Warning: ", LogYellow, title);
+                logColor = LogColor::Yellow;
+                head += std::sprintf(head, "[%s] Warning: ", title);
                 break;
             case LogLevel::Error:
-                std::printf("%s[%s] Error: ", LogRed, title);
+                logColor = LogColor::Red;
+                head += std::sprintf(head, "[%s] Error: ", title);
                 break;
             default:
                 break;
@@ -32,24 +69,31 @@ namespace DX::Internal
 
         va_list args;
         va_start(args, message);
-        vprintf(message, args);
+        head += vsprintf(head, message, args);
         va_end(args);
 
-        std::printf("%s\n", LogNormal);
+        std::sprintf(head, "\n");
+
+        DebugOutput(logColor, buffer);
     }
 
     void Assert(bool condition, const char* title, const char* message, ...)
     {
         if (!condition)
         {
-            std::printf("%s[%s] Assert: ", LogRed, title);
+            char buffer[8 * 1024];
+            char* head = buffer;
+
+            head += std::sprintf(head, "[%s] Assert: ", title);
 
             va_list args;
             va_start(args, message);
-            vprintf(message, args);
+            head += vsprintf(head, message, args);
             va_end(args);
 
-            std::printf("%s\n", LogNormal);
+            std::sprintf(head, "\n");
+
+            DebugOutput(LogColor::Red, buffer);
 
             assert(condition);
         }
