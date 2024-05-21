57d919a4f2868caa7192f3fcb65fcc77dbcae116
AaronRuizMoraUK <aaronruizmora@gmail.com>
2024-05-21 11:08:16 +0100

    Add assimp as 3rdparty library. We'll use it to read 3d files.

--- Scripts/FetchLibraries.cmake
+++ Scripts/FetchLibraries.cmake
@@ -9,6 +9,13 @@ set(3rdparty_folder "3rdParty")
 #
 # Library to handle the window of the application.
 # ---------------------------------------------------------------------
+
+set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Build package with shared libraries.")
+set(GLFW_BUILD_EXAMPLES  OFF CACHE INTERNAL "Build glfw examples.")
+set(GLFW_BUILD_TESTS  OFF CACHE INTERNAL "Build glfw tests.")
+set(GLFW_BUILD_DOCS OFF CACHE INTERNAL "Build glfw docs.")
+set(GLFW_INSTALL  OFF CACHE INTERNAL "Generate installation target.")
+
 FetchContent_Declare(
     glfw
     GIT_REPOSITORY https://github.com/glfw/glfw.git
@@ -17,9 +24,8 @@ FetchContent_Declare(
 
 FetchContent_MakeAvailable(glfw)
 
-set(glfw_folder "${3rdparty_folder}/GLFW3")
+set(glfw_folder "${3rdparty_folder}/glfw")
 set_target_properties(glfw PROPERTIES FOLDER ${glfw_folder})
-set_target_properties(uninstall PROPERTIES FOLDER ${glfw_folder})
 set_target_properties(update_mappings PROPERTIES FOLDER ${glfw_folder})
 
 # ---------------------------------------------------------------------
@@ -83,3 +89,43 @@ target_link_libraries(mathfu INTERFACE vectorial)
 set(mathfu_folder "${3rdparty_folder}/mathfu")
 set_target_properties(mathfu PROPERTIES FOLDER ${mathfu_folder})
 set_target_properties(vectorial PROPERTIES FOLDER ${mathfu_folder})
+
+# ---------------------------------------------------------------------
+# assimp & zlib
+#
+# Library to load various 3d file formats into a shared, in-memory format.
+# ---------------------------------------------------------------------
+
+set(BUILD_SHARED_LIBS OFF CACHE INTERNAL "Build package with shared libraries.")
+set(ASSIMP_BUILD_ZLIB ON CACHE INTERNAL "Build zlib library.")
+set(ASSIMP_BUILD_ASSIMP_TOOLS OFF CACHE INTERNAL "Build supplementary tools.")
+set(ASSIMP_BUILD_SAMPLES OFF CACHE INTERNAL "Build assimp samples.")
+set(ASSIMP_BUILD_TESTS OFF CACHE INTERNAL "Build assimp tests.")
+set(ASSIMP_INSTALL OFF CACHE INTERNAL "Disable this if you want to use assimp as a submodule.")
+set(ASSIMP_INSTALL_PDB OFF CACHE INTERNAL "Install MSVC debug files.")
+
+# Configure assimp importers
+set(ASSIMP_BUILD_ALL_IMPORTERS_BY_DEFAULT FALSE CACHE INTERNAL "Disable all importers.")
+set(ASSIMP_BUILD_FBX_IMPORTER TRUE CACHE INTERNAL "Enable FBX Importer.")
+
+# Configure assimp exporters
+set(ASSIMP_NO_EXPORT TRUE CACHE INTERNAL "Disable assimp's export functionality.")
+if (NOT ASSIMP_NO_EXPORT)
+    set(ASSIMP_BUILD_ALL_EXPORTERS_BY_DEFAULT FALSE CACHE INTERNAL "Disable all exporters.")
+    set(ASSIMP_BUILD_FBX_EXPORTER TRUE CACHE INTERNAL "Enable FBX Exporter.")
+endif()
+
+FetchContent_Declare(
+    assimp
+    GIT_REPOSITORY https://github.com/assimp/assimp.git
+    GIT_TAG v5.4.1
+)
+
+FetchContent_MakeAvailable(assimp)
+
+set(assimp_folder "${3rdparty_folder}/assimp")
+set_target_properties(assimp PROPERTIES FOLDER ${assimp_folder})
+set_target_properties(UpdateAssimpLibsDebugSymbolsAndDLLs PROPERTIES FOLDER ${assimp_folder})
+
+set(zlib_folder "${3rdparty_folder}/zlib")
+set_target_properties(zlibstatic PROPERTIES FOLDER ${zlib_folder})
